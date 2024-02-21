cfa48705ad19eb74f57f312bb3ddfe99cab58b55
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-21 09:32:04 -0300

    Revisao de JS

---
+++ INW/javascript/app.js
@@ -0,0 +1,50 @@
+const prompt = require('prompt-sync')()
+ 
+/*
+function teste(){
+    let nome1 = "caio"
+    console.log(nome1)
+}
+teste()
+*/
+ 
+/*
+printar()
+function printar(){
+    console.log("Hello")
+}
+*/
+ 
+/*
+printar()
+var nome1
+function printar(){
+    console.log(nome1 = "Caio")
+}
+*/
+ 
+/*
+const nome1 = "caio"
+console.log(nome1.toUpperCase())
+*/
+ 
+/*
+const nome1 = "caio"
+console.log(typeof nome1)
+*/
+ 
+/*
+const nome1 = "caio"
+console.log(`Ola ${nome1 === 'caio'? 'Ola' : 'Não te conheço'}`)
+*/
+ 
+/*
+let nome1 = "Caio"
+console.log(nome1.length)
+console.log(nome1.replace('C',"P"))
+*/
+ 
+/*
+let nome1 = "Caio"
+console.log(Math.floor(Math.random()*10))
+*/
