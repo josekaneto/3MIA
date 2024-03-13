c5695b6b410b709630fd3ecae7b5d5e987fb9436
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-13 09:35:49 -0300

    Revisao Array

---
+++ INW/javascript/aula4.js
@@ -0,0 +1,20 @@
+const nomes = [
+    {
+        nomes:'Ryu',
+        tipo: 'vermelho',
+        idade: 187
+    },
+    {
+        nomes:'Akuma',
+        tipo:'fogo',
+        idade: 250
+    }
+]
+
+nomes.filter((element)=>{
+    console.log(element.idade)
+})
+
+
+
+
