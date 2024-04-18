339001bcad3826deebfd52d31654a02c30df72da
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-18 11:32:24 -0300

    Ajustando Cards CSS - React

--- INW/react/At1Tri/At-React/src/Componentes/Cards/Cards.jsx
+++ INW/react/At1Tri/At-React/src/Componentes/Cards/Cards.jsx
@@ -1,32 +1,64 @@
-export default function Cards(){
-    return(
-        <section className="section">
-        <div className="cards">
-            <div className="card1">
-                <p className="numero">01</p>
-                <img src="../../../public/youtube.png" alt="logo yotube" />
-                <h3>Youtube</h3>
-                <p>Produza conteúdo cativante e conquiste seu público com vídeos de alta qualidade no maior palco digital do mundo.</p>
+export default function Cards() {
+  return (
+    <section className="cards-section">
+      <div className="cards">
+        <div className="card1">
+          <div className="numero-logo">
+            <p className="numero">01</p>
+            <div>
+              <img
+                className="imagem"
+                src="../../../public/youtube.png"
+                alt="logo yotube"
+              />
             </div>
-            <div className="card2">
-                <p className="numero">02</p>
-                <img src="../../../public/Tiktok.png" alt="logo tik tok" />
-                <h3>Tik Tok</h3>
-                <p>Faça sua criatividade brilhar em vídeos curtos e envolventes que vão cativar a comunidade global do TikTok.</p>
-            </div>
-            <div className="card3">
-                <p className="numero">03</p>
-                <img src="../../../public/Facebook.png" alt="logo Facebook" />
-                <h3>Facebook</h3>
-                <p>Conecte-se com sua audiência de forma autêntica e impactante através de vídeos que se destacam no feed do Facebook.</p>
-            </div>
-            <div className="card4">
-                <p className="numero">04</p>
-                <img src="../../../public/Instagram.png" alt="logo Instagram" />
-                <h3>Instagram</h3>
-                <p>Compartilhe suas histórias de maneira única e conquiste milhões de likes no Instagram utilizando Stories e Reels.</p>
-            </div>
+          </div>
+          <h3>Youtube</h3>
+          <p>
+            Produza conteúdo cativante e conquiste seu público com vídeos de
+            alta qualidade no maior palco digital do mundo.
+          </p>
         </div>
-        </section>
-    )
+        <div className="card2">
+        <div className="numero-logo">
+          <p className="numero">02</p>
+          <div>
+          <img src="../../../public/Tiktok.png" alt="logo tik tok" />
+          </div>
+          </div>
+          <h3>Tik Tok</h3>
+          <p>
+            Faça sua criatividade brilhar em vídeos curtos e envolventes que vão
+            cativar a comunidade global do TikTok.
+          </p>
+        </div>
+        <div className="card3">
+            <div className="numero-logo">
+             <p className="numero">03</p>
+                <div>
+                <img src="../../../public/Facebook.png" alt="logo Facebook" />
+            </div>
+          </div>
+          <h3>Facebook</h3>
+          <p>
+            Conecte-se com sua audiência de forma autêntica e impactante através
+            de vídeos que se destacam no feed do Facebook.
+          </p>
+        </div>
+        <div className="card4">
+            <div className="numero-logo">
+          <p className="numero">04</p>
+          <div>
+          <img src="../../../public/Instagram.png" alt="logo Instagram" />
+          </div>
+          </div>
+          <h3>Instagram</h3>
+          <p>
+            Compartilhe suas histórias de maneira única e conquiste milhões de
+            likes no Instagram utilizando Stories e Reels.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
