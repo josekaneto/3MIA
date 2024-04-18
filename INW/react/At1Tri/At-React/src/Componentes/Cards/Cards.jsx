5f920e98ee40df62f9568f704c473d8f54f7293c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-18 13:46:20 -0300

    CSS tela sobre - react

--- INW/react/At1Tri/At-React/src/Componentes/Cards/Cards.jsx
+++ INW/react/At1Tri/At-React/src/Componentes/Cards/Cards.jsx
@@ -14,46 +14,46 @@ export default function Cards() {
             </div>
           </div>
           <h3>Youtube</h3>
-          <p>
+          <p className="texto-cards">
             Produza conteúdo cativante e conquiste seu público com vídeos de
             alta qualidade no maior palco digital do mundo.
           </p>
         </div>
         <div className="card2">
-        <div className="numero-logo">
-          <p className="numero">02</p>
-          <div>
-          <img src="../../../public/Tiktok.png" alt="logo tik tok" />
-          </div>
+          <div className="numero-logo">
+            <p className="numero">02</p>
+            <div>
+              <img src="../../../public/Tiktok.png" alt="logo tik tok" />
+            </div>
           </div>
           <h3>Tik Tok</h3>
-          <p>
+          <p className="texto-cards">
             Faça sua criatividade brilhar em vídeos curtos e envolventes que vão
             cativar a comunidade global do TikTok.
           </p>
         </div>
         <div className="card3">
-            <div className="numero-logo">
-             <p className="numero">03</p>
-                <div>
-                <img src="../../../public/Facebook.png" alt="logo Facebook" />
+          <div className="numero-logo">
+            <p className="numero">03</p>
+            <div>
+              <img src="../../../public/Facebook.png" alt="logo Facebook" />
             </div>
           </div>
           <h3>Facebook</h3>
-          <p>
+          <p className="texto-cards">
             Conecte-se com sua audiência de forma autêntica e impactante através
             de vídeos que se destacam no feed do Facebook.
           </p>
         </div>
         <div className="card4">
-            <div className="numero-logo">
-          <p className="numero">04</p>
-          <div>
-          <img src="../../../public/Instagram.png" alt="logo Instagram" />
-          </div>
+          <div className="numero-logo">
+            <p className="numero">04</p>
+            <div>
+              <img src="../../../public/Instagram.png" alt="logo Instagram" />
+            </div>
           </div>
           <h3>Instagram</h3>
-          <p>
+          <p className="texto-cards">
             Compartilhe suas histórias de maneira única e conquiste milhões de
             likes no Instagram utilizando Stories e Reels.
           </p>
--- INW/react/At1Tri/At-React/src/Pages/Sobre.jsx
+++ INW/react/At1Tri/At-React/src/Pages/Sobre.jsx
@@ -1,5 +1,17 @@
 export default function Sobre(){
     return(
-        <h1>Sobre</h1>
+        <section className="section">
+            <div className="div-section-sobre">
+                <div className="div-textos">
+                    <h1 className="titulo">Crie seus vídeos online</h1>
+                    <p className="texto">Transforme suas ideias em filmes memoráveis: onde a criatividade encontra a simplicidade.</p>
+                    <button className="botao" type="button">Começar agora!</button>
+                </div>
+                <div className="div-img">
+                    <img className="img-sobre" src="../../public/celular1.png" alt="" />
+                    <img className="img-sobre" src="../../public/celular2.png" alt="" />
+                </div>
+            </div>
+        </section>
     )
 }
