88c97ac3f812baf51017e767c5cc9449965097a1
José Otávio Kaneto <99183114+josekaneto@users.noreply.github.com>
2024-04-26 13:00:21 -0300

    Ion- range

---
+++ LP3/ion-range/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {}
+  valorRange=0
+
+  onIonChange(ev:RangeCustomEvent){
+    this.valorRange = parseInt(ev.detail.value.toString())
+  }
+
+}
