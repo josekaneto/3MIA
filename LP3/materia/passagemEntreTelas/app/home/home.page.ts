bba75f25905ebf710527149be1127e9b808355be
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-21 13:17:46 -0300

    Matéria - passagem entre telas - Ionic, Agular

---
+++ LP3/materia/passagemEntreTelas/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public router:Router) {}
+
+  nome='';
+  telefone='';
+  email='';
+
+  abrirTela(){
+    this.router.navigateByUrl
+    (`/tela-confirmar/${this.nome}/${this.email}/${this.telefone}`)
+  }
+
+}
