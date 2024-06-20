23529cef4a01406c1bbedd4b424477fafc2a5de1
José Otávio Kaneto <josekaneto0828@gmail.com>
2024-06-19 21:21:54 -0300

    Estilizando tela de contato - React, Tailwind

--- INW/react/Filmes/CineKa/src/Pages/Contato.jsx
+++ INW/react/Filmes/CineKa/src/Pages/Contato.jsx
@@ -1,26 +1,26 @@
 function Contato() {
     return ( 
-        <div className="flex">
+        <div className="flex my-20">
         <div className="w-2/4 p-24">
-            <div className="contato-titulo">
-                <h1>Dúvidas e suporte, entre em contato:</h1>
+            <div className="w-3/4">
+                <h1 className="font-font-sub text-7xl text-white">Dúvidas e suporte, entre em contato:</h1>
             </div>
         </div>
-        <div className=" h-[520px] bg-white w-1"  />
-        <form className="form-contato">
-            <div className="nome">
-                <label className="form-label-nome"  htmlFor="nome">Nome:</label>
-                <input className="form-input-nome" type="text" id="nome"/>
+        <div className="h-auto bg-white w-1 "  />
+        <form className="w-2/4 flex flex-col justify-center items-center">
+            <div className="w-4/5 flex justify-around mb-4">
+                <label className="text-2xl text-white"  htmlFor="nome">Nome:</label>
+                <input placeholder='exemplo' className="w-3/4 rounded-full bg-secondary-kaneto focus:outline-none p-2" type="text" id="nome"/>
             </div>
-            <div className="email">
-                <label className="form-label-email" htmlFor="email">E-mail:</label>
-                <input className="form-input-email" type="email" id="email" />
+            <div className="w-4/5 flex justify-around mb-4">
+                <label className="text-2xl text-white" htmlFor="email">E-mail:</label>
+                <input placeholder="exemplo@gmail.com" className="w-3/4 rounded-full bg-secondary-kaneto focus:outline-none p-2" type="email" id="email" />
             </div>
-            <div className="textarea">
-                <label className="form-label-textarea" htmlFor="mensagem">Mensagem :</label>
-                <textarea className="form-textarea" id="mensagem" cols="30" rows="10"></textarea>
+            <div className="w-[74%] flex flex-col mb-4">
+                <label className="text-2xl text-white mb-4" htmlFor="mensagem">Mensagem :</label>
+                <textarea className="rounded-2xl bg-secondary-kaneto focus:outline-none p-2" id="mensagem" cols="30" rows="10"></textarea>
             </div>
-            <input className="contato-botao" type="button" value="Enviar" />
+            <input className="rounded-lg py-3 px-24 text-white bg-primary-kaneto mt-5" type="button" value="Enviar" />
         </form>
         </div>
     );
