23c09526bc5984fcb9306b82df3eb18720dc1001
José Otávio Kaneto <josekaneto0828@gmail.com>
2024-06-20 00:02:02 -0300

    Finalização tela de Noticias e implementando carousel na tela de filmes - React, Tailwind

--- INW/react/Filmes/CineKa/src/Pages/Filmes.jsx
+++ INW/react/Filmes/CineKa/src/Pages/Filmes.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaArrowAltCircleDown } from "react-icons/fa";
-
+import { Carousel, Modal, Button } from 'antd';
 
 function Filmes() {
 
@@ -14,25 +14,82 @@ function Filmes() {
         .catch(error => console.log(error))
     },[])
 
+    const [slides, setSlides] = useState([])
+
+    useEffect(() =>{
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=7c572a9f5b3ba776080330d23bb76e1e`)
+        .then (Response => Response.json() )
+        .then(response => setSlides(response.results))
+        .catch(error => console.log(error))
+    })
+
+    const [dataModal, setDataModal] = useState({});
+    const [showModal, setShowModal] = useState(false);
+
+    const moreDetail = items => {
+        setShowModal(true);
+        setDataModal(items);
+      };
+    
+      const closeDetail = () => {
+        setShowModal(false);
+      };
+
     return (
         <>
-        <h2 className="text-white text-4xl mb-5 font-font-sub" >Em destaque</h2>
-        <div className="listaFilmes flex flex-row gap-3 flex-wrap">
-            {
-                filmes.map(
-                    filme =>(
-                        <div key={filme.id} className="card-filme bg-white text-black rounded-xl w-[150px]">
-                            <h1 className="text-lg text-center text-titulo-kaneto" key={filmes}>{filme.title}</h1>
-                            <img className="" src={`https://image.tmdb.org/t/p/w154/${filme.poster_path}`} alt="" />
-                            <Link onClick={()=>document.getElementById('my_modal_3').showModal()} className="bg-primary-kaneto text-secondary-kaneto text-xl"><FaArrowAltCircleDown/></Link>
-                        </div>
+
+        <Modal
+          title={dataModal.title}
+          open  = {showModal}
+          footer={
+            <Button  onClick={closeDetail} className="bg-primary-kaneto text-white hover:bg-none hover:border-primary-kaneto">
+              Ok
+            </Button>
+          }
+          onCancel={closeDetail}
+        >
+            <img src={dataModal.image} />
+            <p> {dataModal.text} </p>
+        </Modal>
+
+            {/* <Carousel arrows infinite={false}>
+                {
+                    slides.map(
+                        slide => (
+                                <>
+                                    <img src={`https://image.tmdb.org/t/p/w154/${slide.backdrop_path}`} alt="" />
+                                </>
+                        ) 
                     )
-                )
-            }
-        </div>
+                }
+            </Carousel>  */}
+
+
+        <h2 className="text-white text-4xl mb-5 font-font-sub" >Em destaque</h2>
+            <div className="listaFilmes flex flex-row gap-3 flex-wrap">
+                {
+                    filmes.map(
+                        filme =>(
+                            <>
+                            <Carousel arrows infinite={false} className="text-black">
+                                <div key={filme.id} className="card-filme bg-white text-black rounded-xl w-[150px]">
+                                    <h1 className="text-lg text-center text-titulo-kaneto" key={filmes}>{filme.title}</h1>
+                                    <img className="" src={`https://image.tmdb.org/t/p/w154/${filme.poster_path}`} alt="" />
+                                    <Link onClick={()=> moreDetail(filme)} className="bg-primary-kaneto text-secondary-kaneto text-xl"><FaArrowAltCircleDown/></Link>
+                                </div>
+                            </Carousel>  
+                              </>
+                              
+                            
+                        )
+                    )
+                }
+                
+            </div>
         </>
     );
     
 }
 
 export default Filmes;
+
--- INW/react/Filmes/CineKa/src/Pages/Home.jsx
+++ INW/react/Filmes/CineKa/src/Pages/Home.jsx
@@ -1,18 +1,48 @@
 import  data  from "../../artigos.json";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { Modal, Button } from "antd";
 
 function Home() {
+
+    const [dataModal, setDataModal] = useState({});
+    const [showModal, setShowModal] = useState(false);
+
+    const moreDetail = items => {
+        setShowModal(true);
+        setDataModal(items);
+      };
+    
+      const closeDetail = () => {
+        setShowModal(false);
+      };
+
     return (
+        
         <>
+
+        <Modal
+          title={dataModal.title}
+          open  = {showModal}
+          footer={
+            <Button  onClick={closeDetail} className="bg-primary-kaneto text-white hover:bg-none hover:border-primary-kaneto">
+              Ok
+            </Button>
+          }
+          onCancel={closeDetail}
+        >
+            <img src={dataModal.image} />
+            <p> {dataModal.text} </p>
+        </Modal>
             <h2 className="font-font-sub font-black text-5xl text-white flex justify-center my-10">Cine News</h2>
             <div className="grid grid-cols-2">
             {
                 data.map(
                     (filme, index) => (
                         <>
-                        <div className="card bg-card text-white m-3 flex justify-around rounded-2xl h-[95%]" key={index} >
-                            <img className=" w-2/5 rounded-l-2xl " src={filme.image} alt="" />
-                            <div className="flex flex-col">
+                        <div className="card bg-card text-white m-3 flex rounded-2xl h-[95%]" key={index} >
+                            <div className=" w-2/4 bg-cover rounded-l-2xl" style={{backgroundImage: `url(${filme.image})`}}></div>
+                            <div className="flex flex-col w-2/4">
                                 <h1 className="text-2xl text-center my-2 text-titulo-kaneto"  key={filme.title}>{filme.title}</h1>
                                 <div className="tags flex justify-center" >
                                 {
@@ -23,43 +53,21 @@ function Home() {
                                 </div>
                                 <div className="mx-3 my-3 text-justify" >
                                 {
-                                    filme.text.map( text => (
-                                        <p  className=" text-xs font-font-text" key={text}>{text}</p>
+                                    filme.text.map( (text, index )=> (
+                                        <p  className=" text-xs font-font-text" key={index}>{text}</p>
                                     ))
                                 }
                                 </div>
-                                <NavLink onClick={()=>document.getElementById('my_modal_3').showModal()}  className="text-sm font-font-sub justify-start mx-3 text-hover-color">leia mais</NavLink>
+                                <NavLink onClick={()=> moreDetail(filme)}  className="text-sm font-font-sub justify-start mx-3 text-hover-color">leia mais</NavLink>
                             </div>
                             
                         </div>
 
-                        <dialog  id="my_modal_3" className="modal w-2/5 rounded-2xl bg-black border-2 border-primary-kaneto">
-                            <div className="modal-box">
-                                <form method="dialog">
-                                    <button className="btn btn-2xl text-4xl btn-circle btn-ghost absolute left-2 text-white">🠔</button>
-                                </form>
-                                <img className="p-10" key={filme.image} src={filme.image} alt=""/>
-                                <div className="mx-10 mb-5 text-justify" >
-                                {
-                                    filme.text.map( text => (
-                                        <p  className=" text-xl font-font-text text-white" key={text}>{text}</p>
-                                    ))
-                                }
-                                </div>
-                            </div>
-                        </dialog>
-                        
                         </>
                     )
                     )
-
-                    
                 }
-                
             </div>
-
-            
-
         </>
     );
 }
