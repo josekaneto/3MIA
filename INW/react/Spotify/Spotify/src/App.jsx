6054b5abeb97f9929135485027c61ce0bef242c8
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 08:54:18 -0300

    Relembrando CSS com tailwind - React, Tailwind

---
+++ INW/react/Spotify/Spotify/src/App.jsx
@@ -0,0 +1,72 @@
+function App() {
+
+  return (
+    <>
+      <body className='h-screen'>
+        <header className='bg-pink-700 h-1/5'></header>
+
+        <main className='flex h-4/5'>
+          <div className='bg-cyan-400 w-1/6 h-full flex flex-col p-5 justify-between'>
+            <div className='bg-pink-700 h-32'></div>
+            <div className='bg-pink-700 h-32'></div>
+            <div className='bg-pink-700 h-32'></div>  
+          </div>
+          <div className="grid grid-cols-4 bg-slate-400 w-5/6 p-10 items-center place-items-center">
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center -mt-5'>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center -mt-5'>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center -mt-5'>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center -mt-5'>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center mt-11 '>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center mt-11 '>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center mt-11 '>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center mt-11 '>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center mt-11 '>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center mt-11 '>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center mt-11 '>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+            <div className='bg-pink-700 h-32 w-32 flex flex-col justify-around items-center mt-11 '>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
+            </div>
+
+          </div>
+        </main>
+      </body>
+    </>
+  )
+}
+
+export default App
+
