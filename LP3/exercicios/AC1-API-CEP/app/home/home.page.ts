da2da7457969e2c82431f7e517e2698e61d9e190
José Otávio Kaneto <josekaneto0828@gmail.com>
2024-09-23 20:06:56 -0300

    AC1 puxando API de CEP - Angular, Ionic

---
+++ LP3/exercicios/AC1-API-CEP/app/home/home.page.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  cep: string = '';
+  logradouro: string = '';
+  bairro:string = '';
+  localidade: string = '';
+  uf: string = '';
+  ddd: string = ''
+
+
+  cep2: string = '';
+  uf2: string = '';
+  bairro2: string = '';
+  logradouro2: string = '';
+  localidade2: string = '';
+  ddd2: string = ''
+
+  constructor(private http: HttpClient) {}
+
+  buscarCep() {
+    const url = `https://viacep.com.br/ws/${this.cep}/json/`;
+    if(this.cep !== ''){
+      this.http.get(url).subscribe(
+        (data: any) => {
+        this.logradouro = data.logradouro;
+        this.bairro = data.bairro
+        this.localidade = data.localidade
+        this.uf = data.uf
+        this.ddd = data.ddd
+      },
+      (erro) => {
+        console.error('Erro ao consultar CEP:', erro);
+        alert('Erro ao consultar CEP. Verifique se o CEP está correto.');
+      }
+      )
+    }
+    else{
+      alert('Por favor, insira um CEP válido.');
+    }
+  }
+
+
+  buscarCep2() {
+    const url2 = `https://viacep.com.br/ws/${this.uf2}/${this.localidade2}/${this.logradouro2}/json/`;
+    if(this.uf2 !== '' && this.localidade2 !== '' && this.logradouro2 !== ''){
+    this.http.get(url2).subscribe((data: any) => {
+      this.cep2 = data[0].cep;
+      this.logradouro2 = data[0].logradouro;
+      this.bairro2 = data[0].bairro;
+      this.localidade2 = data[0].localidade;
+      this.uf2 = data[0].uf;
+      this.ddd2 = data[0].ddd
+    },
+    (erro) => {
+      console.error('Erro ao procurar pelo endereço:', erro);
+      alert('Erro ao procurar pelo endereço. Verifique os dados fornecidos.');
+    });
+  }
+  else{
+    alert('Por favor, insira os dados solicitados.');
+  }
+  }
+}
