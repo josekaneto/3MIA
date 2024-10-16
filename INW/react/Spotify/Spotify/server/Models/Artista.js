883d4e515192844c877efb0325eb0171af01284b
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-16 08:47:55 -0300

    Mapeando e filtrando artistras - React, TailwindCSS e MongoDB Atlas

--- INW/react/Spotify/Spotify/server/Models/Artista.js
+++ INW/react/Spotify/Spotify/server/Models/Artista.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const artistaSchema = new mongoose.Schema({
     id:{type: mongoose.Schema.Types.ObjectId},
     name:{type: String, required: true},
+    genero:{type: String}
 })
  
 const artista = mongoose.model('artistas', artistaSchema)
--- INW/react/Spotify/Spotify/src/App.jsx
+++ INW/react/Spotify/Spotify/src/App.jsx
@@ -14,7 +14,8 @@ function App() {
     fetch("http://localhost:3000/artistas")
       .then((res) => res.json())
       .then((data) => setArtistas(data))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(() => console.log("Finalizou a requisição"));
   }, []);
 
   return (
@@ -27,12 +28,40 @@ function App() {
           <CardAside />
         </Aside>
         <ConteudoPrincipal>
-          {artistas.map((artista) => (
-            <div className="bg-pink-700 h-28 w-28 flex flex-col justify-around items-center">
-              <p>{artista.name}</p>
-              <div className="bg-cyan-300 w-4/5 h-8 border-2 border-black "></div>
-            </div>
-          ))}
+          <>
+            <h1>Trap</h1>
+            {artistas
+              .filter((artista) => artista.genero.includes("Trap"))
+              .map((artista) => (
+                <div className="bg-pink-700 h-28 w-28 flex flex-col justify-around items-center">
+                  <h1>{artista.name}</h1>
+                </div>
+              ))}
+            <h1>Rap</h1>
+            {artistas
+              .filter((artista) => artista.genero.includes("Rap"))
+              .map((artista) => (
+                <div className="bg-pink-700 h-28 w-28 flex flex-col justify-around items-center">
+                  <h1>{artista.name}</h1>
+                </div>
+              ))}
+            <h1>Pop</h1>
+            {artistas
+              .filter((artista) => artista.genero.includes("Pop"))
+              .map((artista) => (
+                <div className="bg-pink-700 h-28 w-28 flex flex-col justify-around items-center">
+                  <h1>{artista.name}</h1>
+                </div>
+              ))}
+            <h1>Funk</h1>
+            {artistas
+              .filter((artista) => artista.genero.includes("Funk"))
+              .map((artista) => (
+                <div className="bg-pink-700 h-28 w-28 flex flex-col justify-around items-center">
+                  <h1>{artista.name}</h1>
+                </div>
+              ))}
+          </>
         </ConteudoPrincipal>
       </Container>
     </>
